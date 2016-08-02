a8d0b6acdcd7384ba510025feea776f3b1abe6b3
lonkai <ipavl@softserveinc.com>
2016-08-02 17:52:58 +0300

    02081750

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 // view engine setup
@@ -24,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static('public'));
 
 app.use('/', routes);
-app.use('/users', users);
 
 app.get('/', function (req, res) {
   //res.send('Hello World!');
